7270855a58230b1cbed954eb1ec55111fc2a8a77
Myroslav <myroslavtp@gmail.com>
2022-08-28 11:10:23 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,93 @@
+// Minimum
+
+/*2*/
+
+
+let manFirstName = 'Oleg';
+let manLastName = 'Ivanov';
+let womanFirstName = 'Olga';
+let womanLastName = 'Pavlivna';
+
+// let 2name = 'Oleg';
+// let 2-name = 'Olga';
+
+
+/*3*/
+
+
+//document.getElementById("myH").innerHTML = "My First Page";
+
+/*
+document.getElementById("myH").innerHTML = "My First Page";
+document.getElementById("myP").innerHTML = "My first paragraph.";
+*/
+
+
+/*4*/
+
+// camelCase;
+// snake_case;
+// kebab-case;
+
+
+
+// Norma
+
+
+/*1*/
+
+
+// let accountName = prompt(`Вкажіть ваше ім'я`);
+// alert(`Привіт ` + accountName);
+
+
+/*2*/
+
+
+// let accountYear = +prompt(`Вкажіть ваш рік народження`);
+// const YEAR = 2022;
+// let age = YEAR - accountYear;
+// alert(`Вам ${age} років`);
+
+
+/*3*/
+
+
+// let widthSquare = +prompt('Вкажіть довжину сторони квадрату');
+// let perimetrSquare = widthSquare * 4;
+// alert(`Периметр вашого квадрата ` + perimetrSquare);
+
+
+// Maximum
+
+
+/*1*/
+
+// let circleRadius = +prompt('Вкажцть радіус кола');
+// let circleSquare = 2 * Math.PI * circleRadius;
+// alert(`Площа квадрату дорівнює ` + circleSquare);
+
+
+/*2*/
+
+
+// let distance = +prompt('Вкажіть відстань в кілометрах між двома містами');
+// let time = +prompt('Вкажіть скільки часу плануєте витатити на дорогу');
+// let speed = distance / time;
+// alert(`Швидкість, з якою необхідно рухатися щоб встигнути вчасно = ` + speed);
+
+/*3*/
+
+
+// let dollars = +prompt(`Введіть суму в доларах`);
+// const ONEEURO = 1.0034;
+// let exchange = dollars * ONEEURO;
+// alert(`Ваша сума в євро ` + exchange);
+
+
+
+
+
+
+
+
